a771c04e9f0bbfba259d99503b925a94b844f736
Unknown <jesper.g.hedlund@gmail.com>
2017-01-25 12:25:36 +0100

    Player test

---
+++ Lab10/src/game/Playertest.java
@@ -0,0 +1,19 @@
+package game;
+import game.Player;
+import game.Location;
+
+public class Playertest {
+	public static Location Hell, heaven;
+	public static Player adv;
+	public static void main(String[] args){
+	adv=new Player("Koksnisse");
+	System.out.println(adv.getName());
+	System.out.println(adv.getHealth());
+	adv.changeHealth(6);
+	System.out.println(adv.getHealth());
+	adv.setLoc(Hell);
+	heaven=adv.getLoc();
+	
+	
+	}
+}
