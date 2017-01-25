574767119fdb828324e42ad5e7024338aa77cfa3
Unknown <nilsjonsson@outlook.com>
2017-01-25 10:20:04 +0100

    location

--- Lab1/src/game/Location.java
+++
@@ -1,7 +0,0 @@
-package game;
-
-public class Location {
-public void hej() {
-	System.out.println("tju");
-}
-}
--- Lab10/src/game/Location.java
+++ Lab10/src/game/Location.java
@@ -1,5 +1,29 @@
 package game;
 
 public class Location {
-
+	
+	private String name;
+	private String description;
+	private Location[] paths; 
+	
+	public Location(String name) {
+		this.name = name;
+	}
+	
+	public void introduceYourself() {
+		System.out.println(description);
+	}
+	
+	
+			
+	
+	public void setLocation(Location north, Location east, Location south, Location west) {
+		this.paths[0]=north;
+		this.paths[1]=east;
+		this.paths[2]=south;
+		this.paths[3]=west;
+	}
+	
+	
+	
 }
