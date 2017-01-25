2d38afabaef36d1a16d8ebdad9ba72ba1b9170d5
Unknown <nilsjonsson@outlook.com>
2017-01-25 17:04:19 +0100

    Ny

--- Lab10/src/game/OutdoorsArea.java
+++ Lab10/src/game/OutdoorsArea.java
@@ -4,8 +4,8 @@ import game.Location;
 public class OutdoorsArea extends Location {
 	private String weather;	
 	
-	public OutdoorsArea(String name, String weather) {
-		super(name);
+	public OutdoorsArea(String name, String description, String description2, String weather) {
+		super(name, description, description2);
 		this.weather=weather;
 		
 	}	
